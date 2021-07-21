e0096dbae5973f463e84bf4d52315957ba048c22
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2021-07-20 20:46:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,26 @@ Webcam.set({
 }) 
 camera = document.getElementById("camera");
 Webcam.attach(camera);
+function takePhoto(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("snapshot").innerHTML="<img src='"+data_uri+"' id='picture'>";
+    })
+}
+console.log('ml5:version', ml5.version);
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+function modelLoaded(){
+console.log("model is loaded");
+}
+function checkImage(){
+    img = document.getElementById("picture");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("object_name").innerHTML=result[0].label;
+    }
+}
